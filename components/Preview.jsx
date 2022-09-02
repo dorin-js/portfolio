b5be23542a8f622fbc7c654eac12ae5f9600383a
Dorin Ilusca <dorin.ilusca@hotmail.com>
2022-09-02 22:58:04 +0300

    video loop autoplay fix

--- components/Preview.jsx
+++ components/Preview.jsx
@@ -40,7 +40,7 @@ const Preview = ({ projects }) => {
               </span>
             </div>
           </div>
-          <video width="800" autoPlay muted loop>
+          <video width="800" autoPlay={true} muted={true} loop={true}>
             <source src={`${pr.previewVid}`} type="video/mp4" />
           </video>
           <h3 style={{ margin: "1.2rem 0 0.8rem 0" }}>
